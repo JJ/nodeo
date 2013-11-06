045c92ebfb3c2af544daad44eaacb84b79fb6ac2
JJ Merelo <jjmerelo@gmail.com>
2013-11-06 19:35:01 +0100

    Passes second test

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,8 +1,24 @@
 var test = require('tap').test,
     nodeo = require(__dirname + '/../../lib/nodeo.js');
 
+var population_size = 16;
+var chromosome_size = 16;
+var population = new Array;
 
 test('Loads OK', function (t) {
+	 for ( var i = 0; i < population_size; i ++ )  {
+	     var chromosome = nodeo.random( chromosome_size );
+	     population.push( chromosome );
+	     t.equal(chromosome.length, chromosome_size, "Length " + chromosome + " OK" );
+	     var is_match = chromosome.match(/[01]+/g );
+	     t.ok(is_match, "Binary")	     
+	 }
+
+         t.end();
+     });
+
+
+test('Chromosome', function (t) {
          t.ok(nodeo, 'Loaded OK');
          t.end();
      });
