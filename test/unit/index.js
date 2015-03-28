bf912dbe2fc0c573ee09508d5a7f1440bfeed66b
JJ Merelo <jjmerelo@gmail.com>
2015-03-28 10:01:59 +0100

    Good incorporation, but fails crossover still

--- test/unit/index.js
+++ test/unit/index.js
@@ -5,7 +5,7 @@ var utils = nodeo.utils,
 ops = nodeo.ops; 
 
 var population_size = 16;
-var chromosome_size = 16;
+var chromosome_size = 32;
 var population = new Array([]);
 var fitness_of = new Object({});
 var tournament_size = 3;
@@ -28,7 +28,7 @@ test('chromosomes', function (t) {
 	     var new_chromosome = ops.mutate(chromosome);
 	     t.ok( new_chromosome !== chromosome, "mutation " + chromosome + " - " + new_chromosome);
 	     var to_cross = ops.invert( chromosome );
-	     t.ok( to_cross.charAt(0) !== chromosome.charAt(0), "invert" );
+	     t.ok( to_cross.charAt(0) !== chromosome.charAt(0), "invert" ); // might be the same...
 	     var crossed = ops.crossover( to_cross, chromosome );
 	     t.ok( crossed[0] !== chromosome, "Crossed" );
 	     t.ok( crossed[1] !== to_cross, "Crossed" );
@@ -53,6 +53,9 @@ test('Nodeo', function(t) {
     var the_best_fitness = eo.fitness_of[the_best];
     eo.generation();
     t.ok( eo.fitness_of[eo.population[0]] >= the_best_fitness, "Improving fitness" );
+    var chromosome = utils.random( chromosome_size );
+    eo.incorporate( chromosome );
+    t.equal( new_population.length, population_size, "Size OK");
     do {
 	eo.generation();
     } while ( eo.fitness_of[eo.population[0]] < chromosome_size );
