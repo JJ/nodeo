552ad3b8a80581a21c22ae6bf4032739abcb5013
JJ Merelo <jjmerelo@gmail.com>
2015-11-03 19:48:10 +0100

    Adds maxones

---
+++ app/maxones.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var nodeo = require( "../lib/nodeo.js"),
+Chromosome = nodeo.chromosome,
+maxones = nodeo.utils.max_ones;
+
+function time_maxones(number) {
+    var inicioTiempo = process.hrtime();
+    for ( var i = 0; i < number; i++ ) {
+	var indi = new Chromosome (nodeo.utils.random( length ) );
+	var fitness = maxones( indi )
+    }
+    var finalTiempo = process.hrtime(inicioTiempo);
+    return(finalTiempo[0]+finalTiempo[1]/1e9);
+}
+
+var length = 16
+var iterations = 100000
+var top_length = 65536
+do {
+    console.log("node-BitString, " + length + ", " + time_maxones( iterations ))
+    length = length*2
+} while (length <= top_length);
