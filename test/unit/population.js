5cb6ed09d1d8f9b328af3373c3457486377a88d4
JJ Merelo <jjmerelo@gmail.com>
2016-07-12 10:40:52 +0200

    Working with running population

--- test/unit/population.js
+++ test/unit/population.js
@@ -14,11 +14,13 @@ test('loads', function (t) {
 });
 
 var running_population;
+var chromosome_size = 32;
+var population_size = 32;
+
 test("Creation", function(t) {
     var population = new Population();
     t.type( population, "Population", "is Population" );
-    var chromosome_size = 32;
-    var population_size = 32;
+
     var random_chromosome_32 = function() {
 	return utils.random( chromosome_size );
     };
@@ -34,6 +36,8 @@ console.log( running_population);
 test("Evaluation", function(t) {
     var population = new Population( running_population );
     t.type( population, "Population", "is Population" );
+    t.equal( population.chromosomes().length, population_size, "has been created" );
+    
     t.end();
 });
 
