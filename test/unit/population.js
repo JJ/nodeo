69b6bf58e6182e4133dde36531acd1983ef9380b
JJ Merelo <jjmerelo@gmail.com>
2016-07-12 10:40:10 +0200

    Working with running population

--- test/unit/population.js
+++ test/unit/population.js
@@ -11,8 +11,9 @@ var Population = nodeo.Population,
 test('loads', function (t) {
          t.ok(Population, 'Loaded OK');
          t.end();
-     });
+});
 
+var running_population;
 test("Creation", function(t) {
     var population = new Population();
     t.type( population, "Population", "is Population" );
@@ -22,8 +23,17 @@ test("Creation", function(t) {
 	return utils.random( chromosome_size );
     };
     population.initialize( population_size, random_chromosome_32);
-    t.equal( population.chromosomes().length, chromosome_size, "has been created" );
-    
+    running_population = population.chromosomes();
+    t.equal( running_population.length, population_size, "has been created" );
+
+    t.end();
+});
+
+console.log( running_population);
+
+test("Evaluation", function(t) {
+    var population = new Population( running_population );
+    t.type( population, "Population", "is Population" );
     t.end();
 });
 
