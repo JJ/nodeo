771606343a6e5cc5e5ba9a40ad5710b2e05693d5
JJ Merelo <jjmerelo@gmail.com>
2016-07-12 19:12:51 +0200

    This is almost a miracle
    But now the full algorithm is working

--- test/unit/fluxeo.js
+++ test/unit/fluxeo.js
@@ -3,15 +3,22 @@ var test = require('tap').test,
     nodeo = require(__dirname + '/../../lib/nodeo.js');
 
 var Population = fluxeo.Population,
-    Selection = fluxeo.Selection,
-    Tournament = Selection.Tournament,
-    mmdp = nodeo.MMDP,
-    utils=nodeo.utils;
+Selection = fluxeo.Selection,
+Tournament = Selection.Tournament,
+trap = nodeo.trap,
+utils=nodeo.utils;
 
-var this_mmdp = new mmdp.MMDP();
+var l = 4, b=2;
+
+var this_trap = new trap.Trap( { "l": l, 
+				 "a": 1, 
+				 "b": b, 
+				 "z": 3 });
+
 var this_fitness = function( individual ) {
-    return this_mmdp.apply( individual );
+    return this_trap.apply( individual );
 };
+
 
 test('loads', function (t) {
     t.ok(fluxeo, 'Loaded OK');
@@ -21,16 +28,28 @@ test('loads', function (t) {
 
 var running_population;
 var chromosome_size = 32;
-var population_size = 32;
+var population_size = 256;
 var tournament_size = 2;
 var random_chromosome_32 = function() {
     return utils.random( chromosome_size );
 };
 
+var best_fitness = b*chromosome_size/l;
+console.log( "Best " + best_fitness );
+var solution_found = function ( population ) {
+    if ( population.fitness( population.best() ) >= best_fitness ) {
+	console.log( "It's true" );
+	return true;
+    } else {
+	return false;
+    }
+    
+};
 
 var population = new Population();
 var eo = new fluxeo( this_fitness,
-		     new Tournament( tournament_size, population_size-1 ));
+		     new Tournament( tournament_size, population_size-2 ),
+		     solution_found);
 
 test("Creation", function(t) {
     t.type( population, "Population", "is Population" );
@@ -46,23 +65,35 @@ test("Creation", function(t) {
 
 
 test("Evolution", function(t) {
-    eo.reproduce( population, function( population, new_population ) {
+    eo.create_pool( population, function( population, new_population ) {
 	var this_population = population.chromosomes();
 	t.equal( population.fitness_of[this_population[0]] 
 		 >= population.fitness_of[this_population[1]], true,  "Ranked" );
-	t.equal( new_population.length, population_size-1, "New population");
+	t.equal( new_population.length, population_size-2, "New population");
 	t.end();
     });
 
 });
 
 test("Replacement", function(t) {
-    eo.renew( population, function( population ) {
+    var old_best = population.best();
+    eo.generation( population, function( population ) {
 	t.equal( population.size(), population_size, "Renewed population");
+	t.equal( population.fitness( old_best ) <=
+		 population.fitness( population.best() ), true,
+		 "Improving population" );
 	t.end();
     });
 
 });
 
 
+test("Algorithm", function(t) {
+    eo.algorithm( population, function( population ) {
+	t.equal( population.size(), population_size, "Final population");
+	t.equal( population.fitness( population.best() ),  b* chromosome_size/l, "Best found" );
+	t.end();
+    });
+
+});
 
