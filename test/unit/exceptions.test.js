b03c9ca14ae8d8a1ecd449839ae4331a51e5a4b1
JJ Merelo <jjmerelo@gmail.com>
2023-01-13 08:03:32 +0100

    :recycle: to new API, with new abstract class tested

--- test/unit/exceptions.test.js
+++ test/unit/exceptions.test.js
@@ -1,15 +1,19 @@
-import { ABTException } from "../../lib/ABTException.js";
+import { ABTException } from "../../lib/abt_exception.js";
+import { Selection } from "../../lib/API/selection.js";
 import { test } from "tap";
 
 class Foo {
   constructor() {
-    throw new ABTException(Foo);
+    throw new ABTException("Foo");
   }
 }
 
 test("Exception works as expected", function (t) {
   t.throws(function () {
     new Foo();
+  }, ABTException);
+  t.throws(function () {
+    new Selection();
   }, ABTException);
   t.end();
 });
