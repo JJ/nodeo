95ae7fa15709b410e61f26dedf8f754b9370ff28
JJ Merelo <jjmerelo@gmail.com>
2023-01-19 11:49:47 +0100

    :white_check_mark: #42 with LTrap

--- test/unit/fitness.test.js
+++ test/unit/fitness.test.js
@@ -1,6 +1,7 @@
 import { test } from "tap";
 import {
   AckleyFitness,
+  LTrapFitness,
   RastriginFitness,
   MMDPFitness,
 } from "../../lib/fitness/index.js";
@@ -32,6 +33,18 @@ test("Ackley", function (t) {
       "Ackley " + i + " = " + subjects[i] + " is deterministic "
     );
   }
+  t.end();
+});
+
+test("l-trap", function (t) {
+  let a = 1,
+    b = 2;
+  let params3 = { l: 3, a: a, b: b, z: 2 };
+  let subjects3 = { 111: b, "000": a, "011": 0, 110: 0 };
+  test_trap(t, params3, subjects3);
+  let params4 = { l: 4, a: a, b: b, z: 3 };
+  let subjects4 = { 1111: b, "0000": a, "0111": 0, 1110: 0 };
+  test_trap(t, params4, subjects4);
   t.end();
 });
 
@@ -88,3 +101,20 @@ test("MMDP", function (t) {
 
   t.end();
 });
+
+function test_trap(t, params, subjects) {
+  let local_trap = new LTrapFitness(params);
+  let many_traps = "";
+  let sum = 0;
+  for (let i in subjects) {
+    many_traps += i;
+    sum += subjects[i];
+    t.equal(
+      local_trap.apply(i),
+      subjects[i],
+      "ltrap " + i + " = " + subjects[i]
+    );
+  }
+
+  t.equal(local_trap.apply(many_traps), sum, "Many l-traps");
+}
