849aa1704cce7b3b085abcc2d72b9766501fa335
JJ Merelo <jjmerelo@gmail.com>
2023-07-26 18:11:17 +0200

    :white_check_mark: crossover #44
    The test is probabilistic, because depending on how crossover is made it maybe a no-op

--- test/unit/binary_chromosome.test.js
+++ test/unit/binary_chromosome.test.js
@@ -1,5 +1,6 @@
 import { test } from "tap";
 import { StringChromosome } from "../../lib/chromosomes/string_chromosome.js";
+import _ from "lodash";
 
 const NUMBER_OF_CHROMOSOMES = 32;
 
@@ -18,6 +19,21 @@ test("Generating binary string chromosomes", (t) => {
       const fakeChrom = { stringChr: c };
       t.notSame(c, StringChromosome.mutate(fakeChrom));
     });
+
+    const pairs = _.zip(
+      manyChromosomes.slice(0, NUMBER_OF_CHROMOSOMES / 2),
+      manyChromosomes.slice(NUMBER_OF_CHROMOSOMES / 2)
+    );
+    let diffCrossed = 0;
+    pairs.forEach((p) => {
+      const chrom1 = { stringChr: p[0] };
+      const chrom2 = { stringChr: p[1] };
+      const crossed = StringChromosome.crossover(chrom1, chrom2);
+      if (crossed[0] !== p[0]) {
+        diffCrossed++;
+      }
+    });
+    t.ok(diffCrossed > 1);
   });
   t.end();
 });
