3a5dda40c307556b82558da02bbb51b3f83d84f1
JJ Merelo <jjmerelo@gmail.com>
2023-07-27 12:48:15 +0200

    :white_check_mark: #59

--- test/bun/ops.test.js
+++ test/bun/ops.test.js
@@ -3,6 +3,7 @@ import { expect, test } from "bun:test";
 import { StringChromosome } from "../../lib/chromosomes/string_chromosome.js";
 import { evaluation } from "../../lib/population_ops.js";
 import { LTrapFitness } from "../../lib/fitness/index.js";
+import { Chromosome } from "../../lib/api/chromosome.js";
 
 const NUMBER_OF_CHROMOSOMES = 32;
 const CHROMOSOME_LENGTH = 64;
@@ -15,7 +16,15 @@ const ltrap = new LTrapFitness();
 const manyChromosomes = evaluation(
   StringChromosome,
   manyChromosomeStrings,
-  ltrap.apply
+  (c) => ltrap.apply(c)
 );
 
-console.log(manyChromosomes);
+test("Evaluation with l-trap", function () {
+  expect(manyChromosomes.length).toBe(NUMBER_OF_CHROMOSOMES);
+  manyChromosomes.forEach((c) => {
+    expect(c).toHaveProperty("fitness");
+    expect(c).toHaveProperty("stringChr");
+    expect(c.stringChr.length).toBe(CHROMOSOME_LENGTH);
+    expect(ltrap.apply(c.stringChr)).toBe(c.fitness);
+  });
+});
