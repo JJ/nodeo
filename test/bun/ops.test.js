179a7478faf091d29d0f6bfc07fce7506dcca00e
JJ Merelo <jjmerelo@gmail.com>
2023-07-28 12:22:24 +0200

    Selection refactored and tested :white_check_mark: :recycle: closes #65
    Additionally, testing with bun #59

--- test/bun/ops.test.js
+++ test/bun/ops.test.js
@@ -3,6 +3,7 @@ import { expect, test } from "bun:test";
 import { StringChromosome } from "../../lib/chromosomes/string_chromosome.js";
 import { Evaluation } from "../../lib/population_ops.js";
 import { LTrapFitness } from "../../lib/fitness/index.js";
+import { Tournament } from "../../lib/op/tournament.js";
 
 const NUMBER_OF_CHROMOSOMES = 32;
 const CHROMOSOME_LENGTH = 64;
@@ -14,6 +15,7 @@ const manyChromosomeStrings = StringChromosome.factory(NUMBER_OF_CHROMOSOMES, {
 const ltrap = new LTrapFitness();
 const evaluation = new Evaluation((c) => ltrap.apply(c), StringChromosome);
 const manyChromosomes = evaluation.apply(manyChromosomeStrings);
+const selection = new Tournament(3, NUMBER_OF_CHROMOSOMES);
 
 test("Evaluation with l-trap", function () {
   expect(manyChromosomes.length).toBe(NUMBER_OF_CHROMOSOMES);
@@ -22,5 +24,13 @@ test("Evaluation with l-trap", function () {
     expect(c).toHaveProperty("stringChr");
     expect(c.stringChr.length).toBe(CHROMOSOME_LENGTH);
     expect(ltrap.apply(c.stringChr)).toBe(c.fitness);
+  });
+});
+
+test("tournament selection", function () {
+  const newPopulation = selection.apply(manyChromosomes);
+  expect(newPopulation.length).toBe(NUMBER_OF_CHROMOSOMES);
+  newPopulation.forEach((c) => {
+    expect(c).toHaveProperty("stringChr");
   });
 });
